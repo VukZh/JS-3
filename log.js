4056ead1268f812fbbc854f23971d9418d52c50e
VukZh <36912856+VukZh@users.noreply.github.com>
2018-03-23 11:50:59 +0600

    Replaced Log and Server

--- log.js
+++ log.js
@@ -1,9 +1,12 @@
 const EventEmitter = require('events');
 const logEmitter = new EventEmitter();
 
-log_emit = () => (logEmitter.emit('rqst'));
-log_on = (u) => {
-    logEmitter.once('rqst', () => (console.log('request from URL: ', u)))
+var log_emit = () => (logEmitter.emit('rqst'));
+var log_on = (u1,u2,u3) => {
+    	logEmitter.once('rqst', () => {console.log('request method: ', u1);
+	console.log('request header: ', u2);
+	console.log('request url: ', u3);
+	console.log('');})
 };
 
 module.exports.log_emit = log_emit;
--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const logs = require('./log.js');
 
 const hostname = '127.0.0.1';
 const port = 2000;
-var stringUrl = '';
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Test request event\n');
-    stringUrl = hostname + ':' + port + req.url;
 
 });
 
@@ -17,7 +15,7 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http:${hostname}:${port}/`);
 });
 
-server.on('request', function() {
-    log_on(stringUrl);
-    log_emit();
+server.on('request', function(req,res) {
+    logs.log_on(req.method,req.headers,req.url);
+    logs.log_emit();
 });
